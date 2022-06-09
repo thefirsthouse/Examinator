d733d04944ef81a66503c2e9fc51fbb515514f1e
thefirsthouse <107167088+thefirsthouse@users.noreply.github.com>
2022-06-09 11:29:10 +0300

    Add files via upload

---
+++ Examinator 1.0 (console version).py
@@ -0,0 +1,175 @@
+print("Задание")
+a1 = int(input("Ответ: "))
+
+
+print("Задание")
+a2 = int(input("Ответ: "))
+
+
+
+
+print("Задание")
+a3 = int(input("Ответ: "))
+
+
+print("Задание")
+a4 = int(input("Ответ: "))
+
+
+print("Задание")
+a5 = int(input("Ответ: "))
+
+
+print("Задание")
+a6 = int(input("Ответ: "))
+
+
+print("Задание")
+a7 = int(input("Ответ: "))
+
+
+print("Задание")
+a8 = int(input("Ответ: "))
+
+
+print("Задание")
+a9 = int(input("Ответ: "))
+
+
+print("Задание")
+a10 = int(input("Ответ: "))
+
+
+print("Задание")
+a11 = int(input("Ответ: "))
+
+
+print("Задание")
+a12 = int(input("Ответ: "))
+
+
+print("Задание")
+b1 = int(input("Ответ: "))
+
+
+print("Задание")
+b2 = int(input("Ответ: "))
+
+
+print("Задание")
+b3 = int(input("Ответ: "))
+
+
+print("Задание")
+b4 = int(input("Ответ: "))
+
+
+print("Задание")
+b5 = int(input("Ответ: "))
+
+
+print("Задание")
+c1 = int(input("Ответ: "))
+
+
+print("Задание")
+c2 = int(input("Ответ: "))
+
+print("Задание")
+c3 = int(input("Ответ: "))
+
+
+points = 0
+
+if (a1 == (Ответ)):
+    points += 1
+
+
+if (a2 == (Ответ)):
+    points += 1
+
+
+if (a3 == (Ответ)):
+    points += 1
+
+
+if (a4 == (Ответ)):
+    points += 1
+
+
+if (a5 == (Ответ)):
+    points += 1
+
+
+if (a6 == (Ответ)):
+    points += 1
+
+
+if (a7 == (Ответ)):
+    points += 1
+
+
+if (a8 == (Ответ)):
+    points += 1
+
+
+if (a9 == (Ответ)):
+    points += 1
+
+
+if (a10 == (Ответ)):
+    points += 1
+
+
+if (a11 == (Ответ)):
+    points += 1
+
+
+if (a12 == (Ответ)):
+    points += 1
+
+
+if (b1 == (Ответ)):
+    points += 2
+
+
+if (b2 == (Ответ)):
+    points += 2
+
+
+if (b3 == (Ответ)):
+    points += 2
+
+
+if (b4 == (Ответ)):
+    points += 2
+
+
+if (b5 == (Ответ)):
+    points += 2
+
+
+if (c1 == (Ответ)):
+    points += 3
+
+
+if (c2 == (Ответ)):
+    points += 3
+
+
+if (c3 == (Ответ)):
+    points += 3
+
+
+print("Вы набрали " + str(points) + " баллов")
+
+if (points == 31):
+    print("Выполнив все задания, вы набрали максимальный балл!\n Оценка: 5+")
+elif (points >= 25):
+    print("Оценка: 5")
+elif (points >= 18):
+    print("Оценка: 4")
+elif (points >= 12):
+    print("Оценка: 3")
+else:
+    print("Оценка: 2. Вы отправляетесь на пересдачу.")
